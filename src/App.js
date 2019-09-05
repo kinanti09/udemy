bc5a4851bccc236e3f10d4292920cd85a530d104
kinanti09 <kinantianjang09@gmail.com>
2019-09-05 13:20:25 +0700

    two way binding in react.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 class App extends React.Component {
   constructor(){
     super();
+
+    this.state = {
+      newTodo: 'handle',
+    };
     
     this.state = {
       todos: [{
         id: 1,
-        name: "Play golf"
+        name: "Play golf" 
       },{
         id: 2,
         name: "Buy some clothes"
@@ -21,23 +25,41 @@ class App extends React.Component {
         id: 4,
         name: "Watch bahdcasts"
       }]
-    }
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({
+      newTodo: event.target.value
+    })
+  }
+
   render(){
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="container">
             <h2 className="text-center">Todos App</h2>
+            <input 
+              type="text" 
+              name="todo" 
+              className="form-control"
+              placeholder="Add a new todo" 
+              onChange={this.handlechange}
+              value={this.state.newTodo}
+            />
             <ul className="list-group">
-             {this.state.todos.map((item) => {
-               return <li className="list-groub-item">{item.name}</li>
+             {this.state.todos.map((item, index) => {
+               return <li key={item.id} className="list-groub-item">{item.name}</li>
              })}
              
             </ul>
           </div>
-        </header>
+        </header>    
       </div>
     );
   } 
